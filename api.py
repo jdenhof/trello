41c7c74b05bdaa1fa452b8b4836f3259c101caeb
Jagger Denhof <denhofja@mail.gvsu.edu>
2023-01-25 23:02:48 -0500

    added server

--- api.py
+++ api.py
@@ -15,12 +15,33 @@ query = {
   'token': config.api_token,
 }
 
-def create_new_card(idList, name, due):
+def get_label_id_by_name(boardId = '61f2d57bc0130938a690edd4', name = "personal"):
+	url = 'https://api.trello.com/1/board/{}/labels'.format(boardId)
+
+	response = requests.request(
+		"GET",
+		url,
+		params = query
+	).json()
+	for label in response:
+		if label['name'] == name:
+			return label['id']
+
+def create_idLabels_from_names(names):
+	ids = []
+	for name in names:
+		ids.append(get_label_id_by_name(name = name))
+
+	return ids
+
+def create_new_card(name, due, idLabels, idList = '61f2d57bc0130938a690edd5'):
 	url = 'https://api.trello.com/1/cards'
 
 	query['name'] = name
 	query['due'] = due
 	query['idList'] = idList
+	query['idLabels'] = idLabels
+
 	response = requests.request(
 		"POST",
 		url,
@@ -50,8 +71,5 @@ def get_boards():
 		params=query
 	)
 	print(response.text)
-
-
-create_new_card(queueId, "TESTING", "2017-05-19T14:00:00")
 
 
